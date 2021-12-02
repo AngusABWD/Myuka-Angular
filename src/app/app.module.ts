1f99696b22e0aca61963be4a77dfc5cc56f8bfe0
Angus ABWD <angusabwd@gmail.com>
2021-12-02 11:19:37 +0100

    Import Git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MyukaComponent } from './myuka/myuka.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyukaComponent
   ],
   imports: [
     BrowserModule,
