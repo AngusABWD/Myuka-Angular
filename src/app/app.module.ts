05da8d69360b2b0cacc42125367a59a137283ae1
Angus ABWD <angusabwd@gmail.com>
2021-12-02 10:26:31 +0100

    Import Git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyukaComponent } from './myuka/myuka.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyukaComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/myuka/myuka.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-myuka',
+  templateUrl: './myuka.component.html',
+  styleUrls: ['./myuka.component.css']
+})
+export class MyukaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
